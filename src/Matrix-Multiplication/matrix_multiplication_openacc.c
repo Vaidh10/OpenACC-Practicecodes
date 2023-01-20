974b687ad8528d692c8b9d2194708d8fc18eed5b
Vaidh10 <79251803+Vaidh10@users.noreply.github.com>
2023-01-20 07:46:26 -0500

    Update code

--- src/Matrix-Multiplication/matrix_multiplication_openacc.c
+++ src/Matrix-Multiplication/matrix_multiplication_openacc.c
@@ -9,14 +9,14 @@ int main()
   int first[DIM][DIM], second[DIM][DIM], multiply[DIM][DIM];
  
 #pragma acc parallel loop gang  
-  for ( c = 0 ; c < DIM ; c++ )
+  for ( c = 0 ; c < DIM-1 ; c++ )
     {
 #pragma acc loop worker
-      for ( d = 0 ; d < DIM ; d++ )
+      for ( d = 0 ; d < DIM-1 ; d++ )
       {
         
 #pragma acc loop vector
-	      for ( k = 0 ; k < DIM ; k++ )
+	      for ( k = 0 ; k < DIM-1 ; k++ )
         {
           sum = sum + first[c][k]*second[k][d];
         }
